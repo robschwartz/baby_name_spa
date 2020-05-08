fb285f123a6a0a9cb3f649d62bb58db0744a94a0
Rob Schwartz <rob.schwartz@vibes.com>
2020-05-08 09:00:37 -0400

    update: added list_id to api call to create baby name, clear input after subitting name

--- src/components/NamesContainer.js
+++ src/components/NamesContainer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import update from 'immutability-helper'
+
 
 class NamesContainer extends Component {
   constructor(props) {
@@ -7,28 +9,55 @@ class NamesContainer extends Component {
     this.state = {
       names: [],
       listId: props.listId,
+      inputValue: ''
     }
   }
 
   getNames() {
     axios.get(`/api/v1/baby_names?list_id=${this.state.listId}`)
       .then(response => {
-        this.setState({ names: response.data })
+        this.setState({ names: response.data.baby_names, listId: response.data.list.id })
       })
       .catch(error => console.log(error))
+  }
+
+  createName = (e) => {
+    if (e.key === 'Enter') {
+      axios.post('/api/v1/baby_names', { baby_name: { name: e.target.value, list_id: this.state.listId } })
+        .then(response => {
+          console.log(response.data)
+          const names = update(this.state.names, {
+            $splice: [[0, 0, response.data]]
+          })
+          console.log()
+          this.setState({
+            names: names,
+            inputValue: ''
+          })
+        })
+        .catch(error => console.log(error))
+    }
   }
 
   componentDidMount() {
     this.getNames()
   }
 
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  }
+
   render() {
     return (
       <div>
         <div className="inputContainer">
-          <input className="nameInput" type="text"
-            placeholder="Add a Name" maxLength="50"
-            onKeyPress={this.createName} />
+          <input className="nameInput"
+            type="text"
+            placeholder="Add a Name"
+            maxLength="50"
+            value={this.state.inputValue}
+            onKeyPress={this.createName}
+            onChange={this.handleChange} />
         </div>
         <div className="listWrapper">
           <ul className="nameList">
