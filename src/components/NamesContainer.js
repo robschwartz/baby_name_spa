5a322de3337dad1c65a281574ab103ce310eb2c7
Rob Schwartz <rob.schwartz@vibes.com>
2020-05-08 07:44:17 -0400

    new: added basic function, added function to get list of names

--- src/components/NamesContainer.js
+++ src/components/NamesContainer.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class NamesContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      names: []
+    }
+  }
+
+  getNames() {
+    axios.get('/api/v1/baby_names')
+      .then(response => {
+        this.setState({ names: response.data })
+      })
+      .catch(error => console.log(error))
+  }
+
+  componentDidMount() {
+    this.getNames()
+  }
+
   render() {
     return (
       <div>
         <div className="inputContainer">
           <input className="nameInput" type="text"
-            placeholder="Add a Name" maxLength="50" />
+            placeholder="Add a Name" maxLength="50"
+            onKeyPress={this.createName} />
         </div>
         <div className="listWrapper">
           <ul className="nameList">
+            {this.state.names.map((name) => {
+              return (
+                <li className="name" name={name} key={name.id}>
+                  <label className="nameLabel">{name.name}</label>
+                </li>
+              )
+            })}
           </ul>
         </div>
       </div>
